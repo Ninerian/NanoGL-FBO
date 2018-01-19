9653b16b8d16da8d9b9418e2381c44694d661004
Daniel Große <daniel.grosse@diginetmedia.de>
2018-01-19 16:59:39 +0100

    Changes modules, adds circle

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const Renderer = require('nanogl-renderer');
-const Program = require('nanogl').Program;
-const Circle = require('nanogl-primitives-2d').Circle;
+const glsl = require('glslify')
+const Renderer = require('nanogl-renderer')
+const Program = require('nanogl').Program
+const Circle = require('nanogl-primitives-2d').Circle
 
-const frag = require('./glsl/shader.frag');
-const vert = require('./glsl/shader.vert');
+const frag = glsl('./glsl/shader.frag')
+const vert = glsl('./glsl/shader.vert')
 
 const MyRenderer = Renderer({
-  init() {
-    const gl = this.gl;
-    this.p = new Program(gl);
-    this.p.compile(vert, frag);
-    this.circle = new Circle(gl);
+  init () {
+    const gl = this.gl
+    this.p = new Program(gl)
+    this.p.compile(vert, frag)
+    this.circle = new Circle(gl, 0.5)
   },
 
-  getContextOptions() {
+  getContextOptions () {
     return {
       alpha: true,
       depth: true,
@@ -22,13 +23,19 @@ const MyRenderer = Renderer({
       premultipliedAlpha: true,
       preserveDrawingBuffer: false,
       preferLowPowerToHighPerformance: true,
-      failIfMajorPerformanceCaveat: false,
-    };
+      failIfMajorPerformanceCaveat: false
+    }
   },
 
-  render(dt) {
-    console.log(dt);
+  render (dt) {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT)
+    this.p.use()
+    this.circle.attribPointer(this.p)
+    this.circle.render()
   },
-});
+  pixelRatio: 1.0
+})
 
-var renderer = new MyRenderer(canvas);
+var renderer = new MyRenderer(canvas)
+
+renderer.play()
